7cabadab19bcb7fac31ee1e20b8a87b840756e9a
John Mellet <JohnMellet@outlook.com>
2024-04-23 12:09:00 +0100

    Declared form variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const userEmail = document.getElementById("user-email");
+const userNumber = document.getElementById("user-phone-number");
+const password = document.getElementById("user-password");
+const confirmPassword = document.getElementById("user-password-confirm");
