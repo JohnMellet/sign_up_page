a9ec70b086e61a2f2bc39e66a93e37fbda3f15e1
John Mellet <JohnMellet@outlook.com>
2024-04-23 12:40:54 +0100

    Added event listener 'submit' - validates the input and prevents defaukt behaviour

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const signUpForm = document.getElementById("sign-up-form");
 const firstName = document.getElementById("first-name");
 const lastName = document.getElementById("last-name");
 const userEmail = document.getElementById("user-email");
 const userNumber = document.getElementById("user-phone-number");
 const password = document.getElementById("user-password");
 const confirmPassword = document.getElementById("user-password-confirm");
+
+signUpForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  validateInputs();
+});
